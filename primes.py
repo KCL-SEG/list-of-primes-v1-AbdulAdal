2dffa7340a783f638bc4baf8d71f88a4c26daf67
AbdulAdal <k21007037@kcl.ac.uk>
2022-11-06 15:57:56 +0000

    Implement the primes 1

--- primes.py
+++ primes.py
@@ -3,4 +3,12 @@
 
 def primes(number_of_primes):
     list = []
+
+    for num in range(2,101):
+        prime = True
+        for i in range(2,num):
+            if (num%i==0):
+                prime = False
+        if prime and len(list)<number_of_primes:
+                list.append(num)
     return list
